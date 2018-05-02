0da737fdfd26a0bc8dc38194302395034843756b
eph6 <eph6@pc2-130-l.cs.st-andrews.ac.uk>
2018-05-02 16:55:53 +0100

    added jquery studff and ability to move divs

--- game.js
+++ game.js
@@ -2,8 +2,22 @@ var myVar;
 operation();
 
 function operation() {
-    $('#blueBox').css({
-    left: 1690px,
-    top: 182px
-});
+    $("#blueBox").css({top: 200, left: 200, position:'absolute'});
 }
+
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+        $("#blueBox").css({top: 200, left: 200, position:'absolute'});
+            break;
+        case 38:
+            alert('up');
+            break;
+        case 39:
+            alert('right');
+            break;
+        case 40:
+            alert('down');
+            break;
+    }
+};
