993d24dcde71e562da001d04f242868d5bd4a96c
eddie9802 <eph6@st-andrews.ac.uk>
2018-05-03 21:51:11 +0100

    added comments

--- game.js
+++ game.js
@@ -1,26 +1,23 @@
-operation();
+$("#blueBox").css({top: 200, left: 200, position:'absolute'});
 
-function operation() {
-    $("#blueBox").css({top: 200, left: 200, position:'absolute'});
-}
-
+// Moves blue box in direction of arrow key
 document.onkeydown = function(e) {
     var blueBoxOffset = $("#blueBox").offset();
-    var moveLength = 10;
+    var moveLength = 5;
     switch (e.keyCode) {
-        case 37:
+        case 37: // Move left
             var moveLeft = blueBoxOffset.left - moveLength;
             $("#blueBox").css({top: blueBoxOffset.top, left: moveLeft, position: 'absolute'});
             break;
-        case 38:
+        case 38: // Move up
             var moveUp = blueBoxOffset.top - moveLength;
             $("#blueBox").css({top: moveUp, left: blueBoxOffset.left, position: 'absolute'});
             break;
-        case 39:
+        case 39: // Move right
             var moveRight = blueBoxOffset.left + moveLength;
             $("#blueBox").css({top: blueBoxOffset.top, left: moveRight, position: 'absolute'});
             break;
-        case 40:
+        case 40: // Move down
             var moveDown = blueBoxOffset.top + moveLength;
             $("#blueBox").css({top: moveDown, left: blueBoxOffset.left, position: 'absolute'});
             break;
